aaa5fd79bfcf89e9a0925aef12240f2d11e63fa6
Mimi Vo <mimzy414@gmail.com>
2021-08-24 13:07:14 -0500

    apollo client and revised function

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,64 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { setContext } from "@apollo/client/link/context";
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { createHttpLink, InMemoryCache } from '@apollo/client/core';
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+      });
+
+// function App() {
+//   return (
+//     <Router>
+//       <>
+//         <Navbar />
+//         <Switch>
+//           <Route exact path='/' component={SearchBooks} />
+//           <Route exact path='/saved' component={SavedBooks} />
+//           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+//         </Switch>
+//       </>
+//     </Router>
+//   );
+// }
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={SearchBooks} />
+              <Route exact path='/saved' component={SavedBooks} />
+              <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
-
 export default App;
