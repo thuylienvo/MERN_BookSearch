0c3214203e6e6487d4d3b5914eec9ed0edbbba31
Mimi Vo <mimzy414@gmail.com>
2021-08-24 12:17:48 -0500

    apollo/query type def/resolvers w/mutations

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+    type User {
+        _id: IF
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    input BookDetails {
+        bookId: String!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String
+    }
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!, _id: ID): User
+    }
+    type Mutation {
+        login(email: String, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookDetails): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+
+
+
+
+// export the typeDefs
+module.exports = typeDefs;
